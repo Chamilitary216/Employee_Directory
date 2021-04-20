e968ff97687eb77a8c977320a02c2da1d27f08de
Chris Curry <Chamilitary216@hotmail.com>
2021-04-19 20:39:36 -0400

    styling header

---
+++ reactproject/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import EmployeeContainer from "./components/Container/EmployeeContainer";
+
+const App = () => {
+    return (
+        <>
+        <Header/>
+        <EmployeeContainer/>
+        </>
+    );
+};
+
+export default App;
---
+++ reactproject/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className = "text-center"> Employee Directory</h1>
+
+            <p className ="text-center"> Type name in Search Field.</p>
+        </header>
+    );
+ };
+
+ export default Header;
